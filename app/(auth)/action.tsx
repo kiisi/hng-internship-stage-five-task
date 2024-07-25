deb2087500c1601303f759e35cdba97ec0cce42d
kiisi <kiisifelix06@gmail.com>
2024-07-25 12:05:54 +0100

    remove redirects

--- app/(auth)/action.tsx
+++ app/(auth)/action.tsx
@@ -29,9 +29,6 @@ export async function createAccount(formData: FormDataProps) {
     const payload = {
         email: formData.email!,
         password: formData.password!,
-        options: {
-            emailRedirectTo: 'https://hng-internship-stage-five-task.vercel.app' // you will have to make the project part dynamic in whichever way the framework you are using allows you to do this.
-        }
     }
 
     const supabase = createClient()
