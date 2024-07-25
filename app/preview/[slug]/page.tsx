877e4c8d84376aacc4e06252deed7ee28f557213
kiisi <kiisifelix06@gmail.com>
2024-07-25 13:34:20 +0100

    completed preview navbar

--- app/preview/[slug]/page.tsx
+++ app/preview/[slug]/page.tsx
@@ -1,8 +1,43 @@
-export default function Page(){
+"use client"
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/contexts/user";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+
+    const { slug } = useParams();
+
+    const { user } = useUserContext()
+
+    const userId = user?.id
 
     return (
         <div>
-            Preview Page
-        </div>
+            <div className="p-[24px] bg-primary md:h-[357px] rounded-b-[32px]">
+                <nav className="flex items-center bg-white justify-between p-[16px] bg-white rounded-[12px]">
+                    <div>
+                        {
+                            slug !== userId ? (
+                                <div>&nbsp;</div>
+                            ) : (
+                                <Link href="/">
+                                    <Button variant="secondary" className="px-[16px] md:px-[27px]">
+                                        Back to Editor
+                                    </Button>
+                                </Link>
+                            )
+                        }
+                    </div>
+                    <div>
+                        <Link href={`/preview/${userId}`}>
+                            <Button className="px-[16px] md:px-[27px]">
+                                Share Link
+                            </Button>
+                        </Link>
+                    </div>
+                </nav>
+            </div >
+        </div >
     )
 }
